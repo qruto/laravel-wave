ffdac4c56a1884257c6ec8e7ebfacef61a9cdc9d
Slava Razum <modern.web.artisan@gmail.com>
2022-06-05 15:56:01 +0300

    Remove client event

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Qruto\LaravelWave\Events\ClientEvent;
 use Qruto\LaravelWave\Events\PresenceChannelJoinEvent;
 use Qruto\LaravelWave\Events\PresenceChannelLeaveEvent;
 use Qruto\LaravelWave\Http\Middleware\GenerateSseSocketId;
 use Qruto\LaravelWave\PresenceChannelUsersRepository;
 use Qruto\LaravelWave\Sse\ServerSentEventStream;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth'])->group(function () {
     Route::middleware(GenerateSseSocketId::class)->get(
@@ -21,11 +21,11 @@ Route::middleware(['web', 'auth'])->group(function () {
         /** @var \App\PresenceChannelUsersRepository $store */
         $store = app(PresenceChannelUsersRepository::class);
 
-        if ($store->join(request()->channel, auth()->user(), request()->header('X-Socket-Id'))) {
+        if ($store->join(request()->channel_name, auth()->user(), request()->header('X-Socket-Id'))) {
             broadcast(new PresenceChannelJoinEvent(auth()->user()))->toOthers();
         }
 
-        return response()->json($store->getUsers(request()->channel));
+        return response()->json($store->getUsers(request()->channel_name));
     });
 
     Route::delete('presence-channel-users', function () {
